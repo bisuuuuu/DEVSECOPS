b9c090e6ef20049916dc0e5a98825895322a664c
biswa <biswaprakash1836@gmail.com>
2025-12-17 18:24:42 +0530

    recbubblesort

---
+++ Desktop/bisuu/sorting/src/sorting/recursivebubblesort.java
@@ -0,0 +1,25 @@
+
+package sorting;
+import java.util.Arrays;
+public class recursivebubblesort {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] arr=new int[] {5,3,2,4,1};
+bubble(arr,arr.length);
+System.out.println(Arrays.toString(arr));
+	}
+public static void bubble(int[] arr,int n) {
+	if(n==1) {
+		return;
+	}
+	for(int i=0;i<n-1;i++) {
+			if(arr[i]>arr[i+1]) {
+				int temp=arr[i];
+				arr[i]=arr[i+1];
+				arr[i+1]=temp;
+		}
+	}
+	bubble(arr,n-1);
+	
+}
+}
